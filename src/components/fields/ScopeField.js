6ff68a058a4269fa88de8c41e8e8ce7eb77d1abd
Olli Raitio <olli.raitio@helsinki.fi>
2016-06-08 16:49:57 +0300

    ScopeField accepts * field scope and definition references

--- src/components/fields/ScopeField.js
+++ src/components/fields/ScopeField.js
@@ -12,13 +12,20 @@ import SchemaField from "react-jsonschema-form/lib/components/fields/SchemaField
  *   fieldName: {
  *     fieldValue: {
  *       fields: [<string>] (fields that are shown if fieldName[fieldValue} == true)
+ *       refs: [<string>] (root definitions that are merged recursively to this fieldScope)
  *       uiSchema: <uiSchema> (merged recursively to inner uiSchema
  *       fieldScopes: {fieldName: <fieldScope>, fieldName2 ...}
  *     },
  *     fieldValue2, ...
  *   }
+ *  },
+ *  definitions: {
+ *    defName: <fieldScope>,
+ *    defname2: ...
  *  }
  * }
+ *
+ * Field scope values accept asterisk (*) as field scope selector.
  */
 export default class ScopeField extends Component {
 	static propTypes = {
@@ -48,7 +55,7 @@ export default class ScopeField extends Component {
 				Object.keys(scopes[fieldName]).forEach((fieldValue) => {
 					scopes[fieldName][fieldValue].fields.forEach((scopesFieldName) => {
 						dictionarifiedFieldsInScopes[scopesFieldName] = true;
-					})
+					});
 					if (scopes[fieldName][fieldValue].fieldScopes)
 						getFieldScopesFields(scopes[fieldName][fieldValue]);
 				});
@@ -56,66 +63,53 @@ export default class ScopeField extends Component {
 		}
 		getFieldScopesFields(props.uiSchema["ui:options"]);
 
-		let dictionarifiedSchemaFields = {};
-		Object.keys(props.schema.properties).forEach((fieldName) => {
-			dictionarifiedSchemaFields[fieldName] = true;
-		});
-
-		let fieldsOutsOfScope = [];
-		Object.keys(dictionarifiedSchemaFields).forEach((fieldName) => {
-			if (!dictionarifiedFieldsInScopes[fieldName]) fieldsOutsOfScope.push(fieldName)
-		});
-
-		let schemas = this.getSchemas(props, fieldsOutsOfScope);
+		let schemas = this.getSchemas(props);
 		return {
+			...schemas,
 			primaryfieldsSelector: Object.keys(props.uiSchema["ui:options"].fieldScopes)[0],
-			schema: schemas.schema,
-			uiSchema: schemas.uiSchema
 		};
 	}
 
 	render() {
-		const schema = this.state.schema;
-		let selectField = this.state.primaryfieldsSelector;
-
-		let title = schema.properties[selectField].title ? schema.properties[selectField].title : selectField;
 		return (
 			<SchemaField
-				schema={schema}
-				onChange={this.onChange}
-				formData={this.props.formData}
-				errorSchema={this.props.errorSchema}
-				idSchema={this.props.idSchema}
-				registry={this.props.registry}
-				uiSchema={this.state.uiSchema}
+				{...this.props}
+				{...this.state}
 			/>
 		)
 	}
 
 	// Returns {schema: schema, uiSchema: uiSchema}
-	getSchemas = (props, fieldsOutOfScope) => {
-		let schema = props.schema;
-		let uiSchema = props.uiSchema;
-		let formData = props.formData;
+	getSchemas = (props) => {
+		let {schema, uiSchema, formData, idSchema} = props;
 
 		let options = uiSchema["ui:options"];
 		let generatedUiSchema = options.uiSchema || {};
 
 		let fieldsToShow = {};
-		
-		fieldsOutOfScope.forEach((fieldName) => {
-			fieldsToShow[fieldName] = props.schema.properties[fieldName];
-		})
 
-		function addFieldScopeValues(scopes, fieldSelector, fieldSelectorValue) {
+		const definitions = options.definitions;
+
+		function addFieldSelectorsValues(scopes, fieldSelector, fieldSelectorValue) {
 			let fieldScope = scopes[fieldSelector][fieldSelectorValue];
 			if (!fieldScope) return;
+
+			while (fieldScope.refs) {
+				let refs = fieldScope.refs;
+				fieldScope = update(fieldScope, {$merge: {refs: undefined}});
+				refs.forEach(ref => {
+					fieldScope = merge(fieldScope, definitions[ref]);
+				});
+			}
+
 			fieldScope.fields.forEach((fieldName) => {
 				fieldsToShow[fieldName] = schema.properties[fieldName];
 			});
+
 			if (fieldScope.uiSchema) {
 				generatedUiSchema = merge(generatedUiSchema, fieldScope.uiSchema);
 			}
+
 			if (fieldScope.fieldScopes) {
 				addFieldScopeFieldsToFieldsToShow(fieldScope)
 			}
@@ -126,13 +120,14 @@ export default class ScopeField extends Component {
 			let scopes = fieldScope.fieldScopes;
 			Object.keys(scopes).forEach((fieldSelector) => {
 				fieldsToShow[fieldSelector] = schema.properties[fieldSelector];
-				let fieldSelectorValue = formData[fieldSelector];
-				if (fieldSelectorValue !== undefined) {
-					if (!Array.isArray(fieldSelectorValue))  fieldSelectorValue = [fieldSelectorValue];
-					fieldSelectorValue.forEach(fieldSelectorValue => {
-						addFieldScopeValues(scopes, fieldSelector, fieldSelectorValue);
-					})
-				}
+				let fieldSelectorValues = formData[fieldSelector];
+				if (!Array.isArray(fieldSelectorValues))  fieldSelectorValues = [fieldSelectorValues];
+				fieldSelectorValues.push("*");
+				fieldSelectorValues.forEach(fieldSelectorValue => {
+					if (fieldSelectorValue !== undefined) {
+						addFieldSelectorsValues(scopes, fieldSelector, fieldSelectorValue);
+					}
+				});
 			});
 		}
 		addFieldScopeFieldsToFieldsToShow(options);
@@ -141,9 +136,16 @@ export default class ScopeField extends Component {
 
 		if (uiSchema["ui:options"] && uiSchema["ui:options"].innerUiField) uiOptions.innerUiField = uiSchema["ui:options"].innerUiField;
 
+		schema = update(schema, {$merge: {properties: fieldsToShow}});
+
+		Object.keys(schema.properties).forEach(property => {
+			idSchema = update(idSchema, {$merge: {[property]: {id: idSchema.id + "_" + property}}});
+		});
+
 		return {
-			schema: update(schema, {$merge: {properties: fieldsToShow}}),
-			uiSchema: generatedUiSchema
+			schema: schema,
+			uiSchema: generatedUiSchema,
+			idSchema: idSchema
 		}
 	}
 
