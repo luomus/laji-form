cfccc29bea01c6d7de766e292232611965060763
Olli Raitio <olli.raitio@helsinki.fi>
2016-06-14 12:32:01 +0300

    rm delay from playground app

--- playground/app.js
+++ playground/app.js
@@ -12,18 +12,12 @@ class LajiFormApp extends Component {
 		super(props);
 		this.state = {
 			...schemas,
-			 formData: undefined,
+			 //formData: undefined,
 			onChange: this.onChange,
 			apiClient: new ApiClientImplementation("https://apitest.laji.fi/v0", properties.accessToken, properties.userToken)
 		}
 	}
 
-	componentDidMount(props) {
-		setTimeout(() => {
-			this.setState({formData: schemas.formData});
-		}, 2000);
-	}
-	
 	onChange = (formData) => {
 		this.setState(formData);
 	}
--- src/components/fields/MapArrayField.js
+++ src/components/fields/MapArrayField.js
@@ -65,7 +65,6 @@ export default class MapArrayField extends Component {
 	}
 
 	setActive = id => {
-		console.log(id);
 		this.setState({activeId: id});
 	}
 
