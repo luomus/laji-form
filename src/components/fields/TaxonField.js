af3b28b1e6f7cf932d1391f7825a6b0fa44718c6
Ville-Matti Riihikoki <ville-matti.riihikoski@helsinki.fi>
2016-06-20 14:43:01 +0300

    changed toxon link to point to the actual full uri

--- src/components/fields/TaxonField.js
+++ src/components/fields/TaxonField.js
@@ -64,7 +64,7 @@ export class TaxonWidgetField extends Component {
 			<SchemaField name={"ei"} {...this.props} {...this.state} />
 			<div className="taxon-widget-meta">
 				{options.taxonID ? (<div><span className="taxon-widget-known">Tunnettu nimi</span><br /></div>) : null}
-				<a href={"http://laji.fi/taksoni/" + options.taxonID} target="_blank">{(this.state.urlTxt || options.taxonID)}</a>
+				<a href={"http://tun.fi/" + options.taxonID} target="_blank">{(this.state.urlTxt || options.taxonID)}</a>
 			</div>
 		</div>
 	}
