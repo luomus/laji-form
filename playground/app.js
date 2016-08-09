df975baf11dc6635c8b0f066fa55f771ac46506d
Olli Raitio <olli.raitio@helsinki.fi>
2016-08-09 13:54:10 +0300

    playground fetches schemas from api

--- playground/app.js
+++ playground/app.js
@@ -8,18 +8,30 @@ import ApiClientImplementation from "./ApiClientImplementation";
 import "../src/styles";
 import "./styles.css";
 
+const USE_LOCAL_SCHEMAS = false;
+
 class LajiFormApp extends Component {
 	constructor(props) {
 		super(props);
 		let lang = "fi";
 		this.state = {
 			...schemas,
-			//formData: {gatheringEvent: {leg: [properties.userToken]}, editors: [properties.userToken]},
+			formData: {gatheringEvent: {leg: [properties.userToken]}, editors: [properties.userToken]},
 			onChange: this.onChange,
 			onSubmit: this.onSubmit,
 			apiClient: new ApiClientImplementation("https://apitest.laji.fi/v0", properties.accessToken, properties.userToken, lang),
 			lang: lang
 		}
+	}
+
+	componentDidMount() {
+		if (USE_LOCAL_SCHEMAS) return;
+		this.state.apiClient.fetch("/forms/JX.519", {lang: this.state.lang}).then(result => {
+			const {schema, uiSchema} = result;
+			this.setState({schema, uiSchema});
+		}).catch(() => {
+			console.log("Form request failed - using local schemas.");
+		})
 	}
 
 	onSubmit = ({formData}) => {
