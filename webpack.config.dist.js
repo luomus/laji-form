3f2681856df984690c7b0643383361e23058448e
Olli Raitio <olli.raitio@helsinki.fi>
2016-08-17 17:00:46 +0300

    update packages list and clean webpack.config.dist.js

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -3,7 +3,6 @@ var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	//entry: path.join(__dirname, "src", "components", "LajiForm"),
 	entry: {
 		"laji-form": "./src/components/LajiForm",
 		styles: "./src/styles"
@@ -45,24 +44,15 @@ module.exports = {
 				test: /\.jpg$/,
 				loader: "file-loader?name=images/[name].[ext]"
 			},
-			// Extract css files
 			{
 				test: /\.css$/,
 				loader: ExtractTextPlugin.extract("css-loader")
-				//include: [
-					//path.join(__dirname, "src"),
-					//path.join(__dirname, "node_modules")
-					//path.join(__dirname, "node_modules", "rc-switch", "assets", "index.css")
-				//]
 			},
-			// Optionally extract less files
-			// or any other compile-to-css language
 			{
 				test: /\.less$/,
 				loader: ExtractTextPlugin.extract("css-loader!less-loader")
 			},
 			{ test: /\.gif$/, loader: "url-loader?mimetype=image/png" },
-			{ test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
 			{ test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=images/[name].[ext]" },
 		]
 	}
