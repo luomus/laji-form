6b94920a36fc193f382cf5cc77ca3e4b65778e7e
Olli Raitio <olli.raitio@helsinki.fi>
2016-09-08 13:03:40 +0300

    import React in src/app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import LajiForm from "./components/LajiForm";
+import React from "react";
 import { render } from "react-dom"
 
 export default props => render(<LajiForm {...props} />, props.rootElem);
