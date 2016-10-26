1398f410591e96bf9b609f784177ac1584686149
Olli Raitio <olli.raitio@helsinki.fi>
2016-10-26 17:02:20 +0300

    update grid layout

--- src/components/fields/GridLayoutField.js
+++ src/components/fields/GridLayoutField.js
@@ -3,18 +3,17 @@ import update from "react-addons-update";
 import TitleField from "react-jsonschema-form/lib/components/fields/TitleField";
 import { toIdSchema, shouldRender } from  "react-jsonschema-form/lib/utils"
 import { isHidden } from "../../utils";
-import { Row } from "react-bootstrap";
+import { Row , Col } from "react-bootstrap";
 
 export default class GridLayoutField extends Component {
 	static propTypes = {
 		uiSchema: PropTypes.shape({
 			"ui:options": PropTypes.shape({
-				colType: PropTypes.oneOf(["lg", "md", "sm", "xs"]),
-				maxItemsPerRow: PropTypes.number,
-				showLabels: PropTypes.boolean,
-				limitWidthAlways: PropTypes.boolean,
-				minWidth: PropTypes.number,
-				maxWidth: PropTypes.number
+				lg: PropTypes.integer,
+				md: PropTypes.integer,
+				sm: PropTypes.integer,
+				xs: PropTypes.integer,
+				showLabels: PropTypes.boolean
 			})
 		}).isRequired
 	}
@@ -49,32 +48,20 @@ export default class GridLayoutField extends Component {
 		let groupIdx = 0;
 
 		const options = props.uiSchema["ui:options"];
-		const colType = (options && options.colType) ? options.colType : "md";
-		const maxItemsPerRow = (options && options.maxItemsPerRow
-			&& options.maxItemsPerRow > 0 && options.maxItemsPerRow <= 12) ? options.maxItemsPerRow : 6;
 		const showLabels = (options && options.hasOwnProperty("showLabels")) ? options.showLabels : true;
-		const limitWidth = (options && options.hasOwnProperty("limitWidthAlways")) ? options.limitWidthAlways : false;
-		const neverLimitWidth = (options && options.hasOwnProperty("neverLimitWidth")) ? options.neverLimitWidth : false;
-		const minWidth = (options && options.hasOwnProperty("minWidth")) ? options.minWidth : 2;
-		const maxWidthFromProps = (options && options.hasOwnProperty("maxWidth")) ? options.maxWidth : 12;
 
 		Object.keys(props.schema.properties).forEach(property => {
 			const type = props.schema.properties[property].type;
 			const shouldHaveOwnRow = (type === "array" || type === "object");
 
 			if (isHidden(props.uiSchema, property)) return;
-			if (shouldHaveOwnRow || (groups[groupIdx] && groups[groupIdx].length >= maxItemsPerRow)) groupIdx++;
+			if (shouldHaveOwnRow) groupIdx++;
 			if (!groups[groupIdx]) groups[groupIdx] = [];
 			groups[groupIdx].push(property);
 			if (shouldHaveOwnRow) groupIdx++;
 		});
 
-		const maxWidth = Math.min(
-			parseInt(12 / groups.reduce((max, group) => (max !== undefined) ? Math.max(max, group.length) : group.length, 0)),
-			maxWidthFromProps
-		);
-
-		return {...fieldProps, colType, groups, showLabels, limitWidth, neverLimitWidth, maxWidth, minWidth};
+		return {...fieldProps, groups, showLabels};
 	}
 
 	shouldComponentUpdate(nextProps, nextState) {
@@ -93,37 +80,31 @@ export default class GridLayoutField extends Component {
 		let fields = [];
 		this.state.groups.forEach(group => {
 
-			const baseDivision = parseInt(12 / group.length);
-
 			const SchemaField = this.state.registry.fields.SchemaField;
 
 			group.forEach((property, gi) => {
-				let division = baseDivision;
-				if (!gi && group.length > 1) {
-					division = 12 - ((Object.keys(group).length - 1) * division);
-				}
-
 				const type = this.state.schema.properties[property].type;
 
-				if (!this.state.neverLimitWidth && (this.state.limitWidth ||
-						(type !== "array" && type !== "object" &&
-							(!this.state.uiSchema[property] || !this.state.uiSchema[property]["ui:widget"] ||
-							 this.state.uiSchema[property]["ui:widget"] !== "separatedDateTime")))) {
-					division = Math.min(this.state.minWidth, division);
-				}
+				//if (type !== "array" && type !== "object") {
+				//	division = Math.min(this.state.maxWidth, division);
+				//}
+				const ownRow = (type === "array" || type === "object");
+				const lg = ownRow ? 12 : this.props.uiSchema["ui:options"].lg;
+				const md = ownRow ? 12 : this.props.uiSchema["ui:options"].md;
+				const sm = ownRow ? 12 : this.props.uiSchema["ui:options"].sm;
+				const xs = ownRow ? 12 : this.props.uiSchema["ui:options"].xs;
 
-				if (type !== "array" && type !== "object") {
-					division = Math.min(this.state.maxWidth, division);
-				}
-
-				//const name = this.state.showLabels ? (this.state.schema.properties[property].title || property) : undefined;
 				const title = this.state.schema.properties[property].title ;
 				const name = this.state.showLabels ? (title !== undefined ? title : property) : undefined;
 				let schema = this.state.schema.properties[property];
 				if (!this.state.showLabels) schema = update(schema, {title: {$set: undefined}});
 
 				if (!isHidden(this.state.uiSchema, property)) fields.push(
-					<div key={"div_" + i} className={"col-" + this.state.colType + "-" + division}>
+					<Col key={"div_" + i}
+					     lg={lg}
+					     md={md}
+					     sm={sm}
+					     xs={xs}>
 						<SchemaField
 							key={i}
 							name={name}
@@ -139,7 +120,7 @@ export default class GridLayoutField extends Component {
 									props.onChange(formData);
 								}}
 						/>
-					</div>
+					</Col>
 				)
 				i++;
 			})
