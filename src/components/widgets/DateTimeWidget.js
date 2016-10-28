4c2dd9a485c689d77bed2f5b18be2286d1aeb224
Olli Raitio <olli.raitio@helsinki.fi>
2016-10-28 11:15:39 +0300

    date widget value can be emptied

--- src/components/widgets/DateTimeWidget.js
+++ src/components/widgets/DateTimeWidget.js
@@ -112,7 +112,8 @@ export default class DateTimeWidget extends Component {
 				 (this.toggle === "calendar" && (!this.props.value || !this.props.value.includes("T")))) {
 					formattedValue = momentValue.format("YYYY-MM-DD");
 				}
-				onChange(value === null ? null : formattedValue);
+				console.log(value);
+				onChange(!value ? null : formattedValue);
 				this.toggle = undefined;
 				this.timeWritten = false;
 			}}
