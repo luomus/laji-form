d6e7ce16549d5abe4ec5b9ab93cb7d8377ee5554
Olli Raitio <olli.raitio@helsinki.fi>
2017-01-23 13:55:01 +0200

    add moment to lib.moment-wrapped build

--- webpack.config.lib.moment-wrapped.js
+++ webpack.config.lib.moment-wrapped.js
@@ -22,7 +22,11 @@ module.exports = {
     })
 	],
 	target: "node",
-	externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+	externals: [nodeExternals({
+		whitelist: [
+			"moment"
+		]
+	})], // in order to ignore all modules in node_modules folder
 	module: {
 		loaders: [
 			{
