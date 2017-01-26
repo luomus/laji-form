da1e84bc241143358b8fb0dd16bb121daec78508
Olli Raitio <olli.raitio@helsinki.fi>
2017-01-26 13:34:01 +0200

    fix Date- & TimeWidget clearing

--- src/components/widgets/DateWidget.js
+++ src/components/widgets/DateWidget.js
@@ -1,11 +1,12 @@
 import React from "react";
 import moment from "moment";
 import DateTimeWidget from "./DateTimeWidget";
+import { isEmptyString } from "../../utils";
 
 export default (props) => (
 	<DateTimeWidget
 		{...props}
-		onChange={value => props.onChange(value === null ? null : moment(value).format("YYYY-MM-DD"))}
+		onChange={value => props.onChange(isEmptyString(value) ? null : moment(value).format("YYYY-MM-DD"))}
 		time={false}
 	/>
 );
--- src/components/widgets/TimeWidget.js
+++ src/components/widgets/TimeWidget.js
@@ -1,11 +1,12 @@
 import React from "react";
 import moment from "moment";
 import DateTimeWidget from "./DateTimeWidget";
+import { isEmptyString } from "../../utils";
 
 export default (props) => (
 	<DateTimeWidget
 		{...props}
-		onChange={value => props.onChange(value === null ? null : ("T" + moment(value).toISOString().split("T")[1]))}
+		onChange={value => props.onChange(isEmptyString(value)  ? null : ("T" + moment(value).toISOString().split("T")[1]))}
 		calendar={false}
 		value={props.value ? (moment().format("YYYY-MM-DD") + props.value) : null}
 	/>
