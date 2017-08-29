550f34b8ea5ff261bbf49e45279b5fcda32763a5
aorin <aorin@users.noreply.github.com>
2017-08-29 16:28:50 +0300

    strong text option added

--- src/components/widgets/PlainTextWidget.js
+++ src/components/widgets/PlainTextWidget.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { getUiOptions } from "../../utils";
 
 
-function PlainTextWidget({id, value}) {
+function PlainTextWidget(props) {
+	const {id, value} = props;
+	const {strong} = getUiOptions(props);
+
 	return (
     <span className="plainText">
-      {value}
+		{strong ? <strong>{value}</strong> : value}
       <input type="hidden" id={id} value={typeof value === "undefined" ? "" : value} />
     </span>
 	);
