5fd11499fae3fae2f1d28940fe7760839fc73a0b
Olli Raitio <olli.raitio@helsinki.fi>
2017-11-21 14:55:31 +0200

    fix remote validation

--- src/validation.js
+++ src/validation.js
@@ -2,7 +2,7 @@ import lajiValidate from "laji-validate";
 
 export function initializeValidation(apiClient) {
 	lajiValidate.extend(lajiValidate.validators.remote, {
-		fetch: apiClient.fetch
+		fetch: (...params) => apiClient.fetch(...params)
 	});
 }
 
