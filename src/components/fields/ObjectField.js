80ec1d6685542a7b389837fdc367ba8dbcae74a5
Olli Raitio <olli.raitio@helsinki.fi>
2018-01-05 10:42:19 +0200

    fix ImageArrayField for grid layout

--- src/components/fields/ObjectField.js
+++ src/components/fields/ObjectField.js
@@ -153,11 +153,16 @@ function GridTemplate(props) {
 
 function getCols(props, schema, uiSchema, property) {
 	const cols = {lg: 12, md: 12, sm: 12, xs: 12};
-
-	if ((schema.type === "array" && !(schema.items && schema.items.enum && isMultiSelect(schema, uiSchema)
-		|| uiSchema["ui:field"] === "SingleItemArrayField" ||
-		schema.items && schema.items.type === "string")) ||
-			(schema.type === "string" && uiSchema && getNestedUiFieldsList(uiSchema).includes("SelectTreeField"))) {
+	const uiField = uiSchema && uiSchema["ui:field"] ? uiSchema["ui:field"] : undefined;
+	if (
+		schema.type === "array" &&
+		!(
+			schema.items && schema.items.enum && isMultiSelect(schema, uiSchema) ||
+			uiField === "SingleItemArrayField" ||
+			(schema.items && schema.items.type === "string" && uiField !== "ImageArrayField")
+		) ||
+		(schema.type === "string" && uiSchema && getNestedUiFieldsList(uiSchema).includes("SelectTreeField"))
+	) {
 		return cols;
 	}
 
