74290aaff5e0e05426b43d3c9bb1844cf1b71912
Olli Raitio <olli.raitio@helsinki.fi>
2018-07-17 15:43:12 +0300

    add firefox test config (doesn't work though, see https://github.com/angular/protractor/issues/4715)

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,17 +1,48 @@
+// Firefox isn't run default since it has a bug with mousemove (See https://github.com/angular/protractor/issues/4715 )
+
+const [width, height] = [800, 600];
+const common = {
+	shardTestFiles: true,
+	maxInstances: 4
+};
+const chrome = {
+	...common,
+	browserName: "chrome",
+	chromeOptions: {
+		args: ["--headless", "--disable-gpu", `window-size=${width}x${height}`]
+	},
+};
+
+const firefox = {
+	...common,
+	browserName: "firefox",
+	"firefoxOptions": {
+		args: ["--headless", `--width=${width}', '--height=${height}`]
+	},
+	"moz:firefoxOptions": {
+		args: ["--headless", `--width=${width}', '--height=${height}`]
+	}
+};
+
+let multiCapabilities = [chrome, firefox]
+if (process.env.TEST_BROWSER === "chrome") {
+	multiCapabilities = [chrome];
+} else if (process.env.TEST_BROWSER === "firefox") {
+	multiCapabilities = [firefox];
+}
+if (process.env.NO_HEADLESS) multiCapabilities.forEach(capabilities => {
+	const options = [capabilities["chromeOptions"], capabilities["firefoxOptions"], capabilities["moz:firefoxOptions"]];
+	options.filter(o => o).forEach(_options => {
+		_options.args = _options.args.filter(a => a !== "--headless");
+	});
+});
+
 exports.config = {
 	seleniumAddress: "http://localhost:4444/wd/hub",
 	specs: ["test/*-spec.js"],
-	capabilities: {
-		browserName: "chrome",
-		chromeOptions: {
-			args: ["--disable-gpu", "--window-size=800x600"]
-		},
-		shardTestFiles: true,
-		maxInstances: 4
-
-	},
+	multiCapabilities,
 	SELENIUM_PROMISE_MANAGER: false,
-	onPrepare: function() {
+	onPrepare: async () => {
 		browser.waitForAngularEnabled(false);
 		require("babel-register");
 		require("babel-polyfill");
@@ -22,6 +53,9 @@ exports.config = {
 		env.addReporter(new SpecReporter({
 			displayStacktrace: true
 		}));
+
+		// Set manually since Firefox cli size options don't work.
+		await browser.driver.manage().window().setSize(width, height);
 	},
 	afterEach: function() {
 		browser.manage().logs().get("browser").then((browserLog) => {
