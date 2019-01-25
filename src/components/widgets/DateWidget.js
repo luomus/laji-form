cef3890dc03ff301426a1c183be9655b57f2e632
Olli Raitio <olli.raitio@helsinki.fi>
2019-01-25 15:52:28 +0200

    unify DateTime & TimeWidget clearing value (fixes DateTime bug, json schema validation didn't like null value)

--- src/components/widgets/DateWidget.js
+++ src/components/widgets/DateWidget.js
@@ -4,12 +4,13 @@ import DateTimeWidget from "./DateTimeWidget";
 import { isEmptyString } from "../../utils";
 
 const onChange = onChange => value => {
-	onChange(isEmptyString(value)  ? null : (moment(value).format("YYYY-MM-DD")));
+	onChange(isEmptyString(value) ? undefined : (moment(value).format("YYYY-MM-DD")));
 };
 export default (props) => (
 	<DateTimeWidget
 		{...props}
 		onChange={onChange(props.onChange)}
 		time={false}
+		value={props.value ? moment().format("YYYY-MM-DD") : null}
 	/>
 );
