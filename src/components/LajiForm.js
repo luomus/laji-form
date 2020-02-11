2c1cc6093900f0c681cda1a7a5163558e75d318a
aorin <meeri.rannisto@gmail.com>
2020-02-11 18:02:00 +0200

    start InjectTaxonCensusFilterField component

--- src/components/LajiForm.js
+++ src/components/LajiForm.js
@@ -79,6 +79,7 @@ const fields = importLocalComponents("fields", [
 	"AudioArrayField",
 	"FilterArrayField",
 	"MultiAnyToBooleanField",
+	"InjectTaxonCensusFilterField",
 	{"InputTransformerField": "ConditionalOnChangeField"}, // Alias for backward compatibility.
 	{"ConditionalField": "ConditionalUiSchemaField"}, // Alias for backward compatibility.
 	{"UnitRapidField": "UnitShorthandField"}, // Alias for backward compatibility.
