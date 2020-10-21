a53e44821ea949fa479d669371ecca6185b17dd0
Giorgio Persichini <lucia-giorgio@hotmail.it>
2020-10-21 11:56:39 +0300

    [#175199847] - don't show unit id when form is not saved

--- src/components/fields/FakePropertyField.js
+++ src/components/fields/FakePropertyField.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import PropTypes from "prop-types";
 import VirtualSchemaField from "../VirtualSchemaField";
 import { updateSafelyWithJSONPointer } from "../../utils";
+import { immutableDelete } from "../../utils";
 
 @VirtualSchemaField
 export default class FakePropertyField extends Component {
@@ -19,23 +20,27 @@ export default class FakePropertyField extends Component {
 		let properties = props.schema.properties;
 		let uiSchema = props.uiSchema;
 		let formData = props.formData;
-		let data;
 		Object.keys(fields).forEach((prop) => {
 			const {schema, uiSchema: _uiSchema, formData: _formData} = fields[prop];
 			properties = updateSafelyWithJSONPointer(properties, schema, prop);
 			uiSchema = updateSafelyWithJSONPointer(uiSchema, _uiSchema, prop);
 			formData = _formData
 				?  updateSafelyWithJSONPointer(formData, _formData, prop)
-				: formData;
-			data = formData[prop];		
+				: formData;	
+
+			if(!_formData) {
+			properties = immutableDelete(properties, prop);
+			delete uiSchema['ui:options']['fields'][prop];
+			}	
+
 		});
-		return data ? {
+		return {
 			schema: {
 				...props.schema,
 				properties
 			},
 			uiSchema,
 			formData
-		} : undefined;
+		}
 	}
 }
