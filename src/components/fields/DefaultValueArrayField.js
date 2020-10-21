5702ff99ae7937a2d6ee1f938d0c820ddb0a7fde
Olli Raitio <olli.raitio@helsinki.fi>
2020-10-21 17:50:44 +0300

    fix NumberWidget & DefaultValueArrayField for typescript

--- src/components/fields/DefaultValueArrayField.js
+++ src/components/fields/DefaultValueArrayField.js
@@ -1,5 +1,5 @@
-import { Component } from "react";
-import PropTypes from "prop-types";
+import * as React from "react";
+import * as PropTypes from "prop-types";
 import update from "immutability-helper";
 import VirtualSchemaField from "../VirtualSchemaField";
 
@@ -16,7 +16,7 @@ import VirtualSchemaField from "../VirtualSchemaField";
  * }}
  */
 @VirtualSchemaField
-export default class DefaultValueArrayField extends Component {
+export default class DefaultValueArrayField extends React.Component {
 	static propTypes = {
 		uiSchema: PropTypes.shape({
 			"ui:options": PropTypes.shape({
