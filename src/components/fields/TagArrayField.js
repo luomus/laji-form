223a8116a918e81df68eda4eff68423fbec98eb7
Giorgio Persichini <lucia-giorgio@hotmail.it>
2021-02-02 15:24:12 +0200

    copy-paste keywords in the form split by comma and semicolon

--- src/components/fields/TagArrayField.js
+++ src/components/fields/TagArrayField.js
@@ -55,8 +55,14 @@ export class TagInputComponent extends React.Component {
 		const {value} = this.state;
 		const {tags = []} = this.props;
 		const {separatorKeys = ["Enter", ",", ";"]} = getUiOptions(this.props.uiSchema);
-		if (separatorKeys.includes(e.key) && !isEmptyString(value)) {
+
+		if (separatorKeys.includes(e.key) && !isEmptyString(value) && !separatorKeys.some(v => value.includes(v))) {
 			this.props.onChange([...tags, value], "enter");
+			e.stopPropagation();
+			e.preventDefault();
+		} else if (!isEmptyString(value) && separatorKeys.some(v => value.includes(v))) {
+			value = value.split(new RegExp(separatorKeys.join('|'), 'g')).map(val => val.trim());
+			this.props.onChange([...tags, ...value], "enter");
 			e.stopPropagation();
 			e.preventDefault();
 		} else if (e.key === "Backspace" && isEmptyString(value) && tags.length) {
@@ -83,7 +89,6 @@ export class TagInputComponent extends React.Component {
 		if (!isEmptyString(this.state.value)) {
 			this.props.onChange([...(this.props.tags || []), this.state.value], "blur");
 		}
-
 	}
 
 	setInputRef = (ref) => {
