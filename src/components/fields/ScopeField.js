f4ca490bf2faa234367e0508fbdc27a76261e86d
Olli Raitio <olli.raitio@helsinki.fi>
2021-04-07 13:07:52 +0300

    ScopeField toggled glyph fields are shown as active instead of primary

--- src/components/fields/ScopeField.js
+++ src/components/fields/ScopeField.js
@@ -642,7 +642,7 @@ function GlyphField({settings, idSchema, formData, schema, registry, isIncluded,
 		<OverlayTrigger key={property} overlay={tooltip} placement="left">
 			<GlyphButton glyph={glyph}
 									 disabled={hasData}
-									 variant={isIncluded ? "primary" : "default"}
+									 active={isIncluded}
 									 onClick={onButtonClick} />
 		</OverlayTrigger>
 	);
