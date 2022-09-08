509699720d1ae94b6ea27ae11a92c179e52fb22a
Olli Raitio <olli.raitio@helsinki.fi>
2022-09-08 14:28:43 +0300

    fix array item shouldn't show titles by never using 'name' property

--- src/components/fields/SchemaField.js
+++ src/components/fields/SchemaField.js
@@ -152,6 +152,7 @@ export default class _SchemaField extends React.Component {
 		const {SchemaField} = getDefaultRegistry().fields;
 		return <SchemaField
 			{..._props}
+			name={undefined}
 			registry={registry}
 			schema={schema}
 			uiSchema={uiSchema}
