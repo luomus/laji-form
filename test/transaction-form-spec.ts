68249cc1cb54778c0514c4de196e3e297cff8ef6
aorin <meeri.rannisto@gmail.com>
2023-09-18 11:11:04 +0300

    add tests for transaction form

--- test/transaction-form-spec.ts
+++ test/transaction-form-spec.ts
@@ -1,4 +1,5 @@
-import { Form, createForm, EnumWidgetPOI } from "./test-utils";
+import {Form, createForm, EnumWidgetPOI} from "./test-utils";
+import {$, by, protractor} from "protractor";
 
 describe("transaction form (MHL.930)", () => {
 	let form: Form;
@@ -29,5 +30,37 @@ describe("transaction form (MHL.930)", () => {
 		const formData = await form.getChangedData();
 		expect(formData.localPerson).toBe("Test, User");
 		expect(formData.localPersonEmail).toBe("user.test@email.com");
+	});
+
+	describe("specimen fields", () => {
+		it("adds the ids", async () => {
+			const $input = form.$getInputWidget("awayIDs")
+			await $input.sendKeys("abc cde efg");
+			await $input.sendKeys(protractor.Key.TAB);
+
+			let formData = await form.getChangedData();
+			expect(formData.awayIDs).toEqual(["abc", "cde", "efg"]);
+		});
+
+		it("marks all as returned", async () => {
+			const returnAllButton$ = $(".laji-form-multi-tag-array-field-buttons").all(by.tagName("button")).get(0);
+			await returnAllButton$.click();
+
+			const formData = await form.getChangedData();
+			expect(formData.awayIDs).toEqual([]);
+			expect(formData.returnedIDs).toEqual(["abc", "cde", "efg"]);
+		});
+
+		it("marks selected ids as missing", async () => {
+			const markAsMissingButton$ = $(".laji-form-multi-tag-array-field-buttons").all(by.tagName("button")).get(3);
+			await markAsMissingButton$.click();
+
+			const tags$ = form.$locate("returnedIDs").$$(".rw-multiselect-tag");
+			await tags$.get(1).click();
+
+			const formData = await form.getChangedData();
+			expect(formData.returnedIDs).toEqual(["abc", "efg"]);
+			expect(formData.missingIDs).toEqual(["cde"]);
+		});
 	});
 });
