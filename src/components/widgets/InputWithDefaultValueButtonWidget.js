f1a4c00c02c2d5428c20fa4ffc0090afef12fa48
aorin <meeri.rannisto@gmail.com>
2025-09-03 16:40:01 +0300

    add apiQueryForDefaultValue option to InputWithDefaultValueButtonWidget

--- src/components/widgets/InputWithDefaultValueButtonWidget.js
+++ src/components/widgets/InputWithDefaultValueButtonWidget.js
@@ -10,7 +10,14 @@ export default class InputWithDefaultValueButtonWidget extends React.Component {
 		uiSchema: PropTypes.shape({
 			"ui:options": PropTypes.shape({
 				buttonLabel: PropTypes.string.isRequired,
+				buttonVariant: PropTypes.string,
 				contextFieldForDefaultValue: PropTypes.string,
+				apiQueryForDefaultValue: PropTypes.shape({
+					path: PropTypes.string.isRequired,
+					query: PropTypes.object,
+					resultKey: PropTypes.string.isRequired,
+					cache: PropTypes.bool
+				}),
 				onClick: PropTypes.func
 			}).isRequired
 		}).isRequired,
@@ -20,16 +27,21 @@ export default class InputWithDefaultValueButtonWidget extends React.Component {
 		value: PropTypes.string
 	}
 
+	constructor(props) {
+		super(props);
+		this.state = {fetching: false};
+	}
+
 	render() {
 		const {InputGroup, Button} = this.context.theme;
-		const {buttonLabel} = getUiOptions(this.props);
+		const {buttonLabel, buttonVariant} = getUiOptions(this.props);
 		const {disabled, readonly} = this.props;
 
 		return (
 			<InputGroup>
 				<BaseInputTemplate {...this.props} />
 				<InputGroup.Button className={"input-group-button"}>
-					<Button onClick={this.onClick} disabled={disabled || readonly}>
+					<Button onClick={this.onClick} disabled={disabled || readonly || this.state.fetching} variant={buttonVariant}>
 						{buttonLabel}
 					</Button>
 				</InputGroup.Button>
@@ -38,13 +50,28 @@ export default class InputWithDefaultValueButtonWidget extends React.Component {
 	}
 
 	onClick = () => {
-		const {contextFieldForDefaultValue, onClick} = getUiOptions(this.props);
+		const {contextFieldForDefaultValue, apiQueryForDefaultValue, onClick} = getUiOptions(this.props);
 
 		if (contextFieldForDefaultValue) {
 			const uiSchemaContext = this.props.formContext.uiSchemaContext || {};
 			const defaultValue = uiSchemaContext[contextFieldForDefaultValue];
 			this.props.onChange(defaultValue);
+		} else if (apiQueryForDefaultValue) {
+			const {path, query = {}, resultKey, cache = false} = apiQueryForDefaultValue;
+			const apiClient = this.props.formContext.apiClient;
+			const fetch = (cache ? apiClient.fetchCached : apiClient.fetch).bind(apiClient);
+
+			this.setState({fetching: true});
+			return fetch(path, query).then(result => {
+				if (result[resultKey]) {
+					this.props.onChange(result[resultKey]);
+				}
+				this.setState({fetching: false});
+			}).catch(() => {
+				this.setState({fetching: false});
+			});
 		}
+
 		if (onClick) {
 			onClick();
 		}
