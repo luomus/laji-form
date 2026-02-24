luomus/laji-form

commit f93dda63d9aebab57bfd3c5ccb97674ae86a5e67

rm BaseComponent usage from VirtualSchemaField

src/components/VirtualSchemaField.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { getInnerUiSchema, getUiOptions, getReactComponentName } from "../utils";
-import BaseComponent from "./BaseComponent";
 import getContext from "../Context";
 import { FieldProps } from "../types";
 
@@ -24,12 +23,17 @@ interface LajiFormComponentForVirtualSchemaField extends Omit<React.Component<Fi
  */
  
 export default function VirtualSchemaField<LFC extends Constructor<LajiFormComponentForVirtualSchemaField>>(ComposedComponent: LFC) {
-	@BaseComponent
 	class VirtualSchemaField extends ComposedComponent {
 		constructor(...args: any[]) {
 			super(...args);
 			this.getStateFromProps = this.getStateFromProps.bind(this);
+			this.state = this.getStateFromProps(args[0]);
 			this.render = this.render.bind(this);
+		}
+
+		UNSAFE_componentWillReceiveProps = (props: any, nextContext: any) => {
+			const state = this.getStateFromProps(props);
+			if (state) this.setState(state);
 		}
 
 		static displayName = getReactComponentName(ComposedComponent as any);

commit 534e030448e33d07b643b615464b74710968850c

fix VirtualSchemaField overriding child componentWillReceiveProps (regression from f93dda63d)

src/components/VirtualSchemaField.tsx
@@ -32,8 +32,12 @@ export default function VirtualSchemaField<LFC extends Constructor<LajiFormCompo
 		}
 
 		UNSAFE_componentWillReceiveProps = (props: any, nextContext: any) => {
-			const state = this.getStateFromProps(props);
-			if (state) this.setState(state);
+			if (super.UNSAFE_componentWillReceiveProps) {
+				super.UNSAFE_componentWillReceiveProps(props, nextContext);
+			} else if (this.getStateFromProps) {
+				const state = this.getStateFromProps(props);
+				if (state) this.setState(state);
+			}
 		}
 
 		static displayName = getReactComponentName(ComposedComponent as any);