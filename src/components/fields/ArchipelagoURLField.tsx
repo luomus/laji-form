4c3cb8dd5940814ba5f5b3f8a64d7c0ca53bcd2f
rpulkka <robert.pulkka@gmail.com>
2026-07-07 16:17:53 +0300

    Add form data listener to root-instance-service.

--- src/components/fields/ArchipelagoURLField.tsx
+++ src/components/fields/ArchipelagoURLField.tsx
@@ -25,15 +25,33 @@ export default class ArchipelagoURLField extends React.Component<FieldProps> {
 
 	static getName() { return "ArchipelagoURLField"; }
 
+	getStateFromProps(props: FieldProps) {
+		return {
+			rootFormData: props.formContext.services.rootInstance.getFormData()
+		};
+	}
+
+	componentDidMount() {
+		this.props.formContext.services.rootInstance.addFormDataListener(this.onRootFormDataChange);
+	}
+
+	componentWillUnmount() {
+		this.props.formContext.services.rootInstance.removeFormDataListener(this.onRootFormDataChange);
+	}
+
+	onRootFormDataChange = (rootFormData: any) => {
+		this.setState({ rootFormData });
+	};
+
 	render() {
+		const { rootFormData } = this.state as any;
 		const { baseURL } = getUiOptions(this.props.uiSchema);
 		const { translations } = this.props.formContext;
 		const SchemaField = this.props.registry.fields.SchemaField as any;
-		const dateBegin = this.props.formData?.gatheringEvent?.dateBegin;
+		const dateBegin = rootFormData?.gatheringEvent?.dateBegin;
 		const year = Number(dateBegin?.split("-")[0]) || undefined;
-		const namedPlace = this.props.formData?.namedPlaceID;
+		const namedPlace = rootFormData?.namedPlaceID;
 		const url = baseURL + `/project/MHL.1166/stats?tab=statistics&year=${year}&namedPlace=${namedPlace}`;
-
 		return (
 			<>
 				{year !== undefined && namedPlace !== undefined && (
@@ -41,7 +59,7 @@ export default class ArchipelagoURLField extends React.Component<FieldProps> {
 						<b>{translations.ResultsLink}</b>
 						<br />
 						<a href={url} target="_blank">
-							{url}
+							{translations.FieldObservationsFromNP}
 						</a>
 					</div>
 				)}
